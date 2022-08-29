d7df787e663f0c8e62c601aa4d8a8cc62e38f73c
Willian B <speedyb0y@users.noreply.github.com>
2022-08-29 08:17:49 -0300

    Add files via upload

--- xtun-encoding.c
+++ xtun-encoding.c
@@ -1,26 +1,29 @@
 
-#if XGW_XTUN_ENCODING_BYTE_X <= 0
- || XGW_XTUN_ENCODING_BYTE_X >= 0xFF
-#error "BAD BYTE X"
-#endif
-
-#define BYTE_X XGW_XTUN_ENCODING_BYTE_X
+#define XTUN_ENCODING_ORIG_ADD XGW_XTUN_ENCODING_BYTE_X
 
 #define ENCODE_SECRET_ADD XGW_XTUN_ENCODE_SECRET_ADD
 
+#if XTUN_ENCODING_ORIG_ADD <= 0 \
+ || XTUN_ENCODING_ORIG_ADD >= 0xFF
+#error "BAD ORIG ADD"
+#endif
+
+#if ENCODE_SECRET_ADD <= 0 \
+ || ENCODE_SECRET_ADD > 0xFFFFFFFFFFFFFFFF
+#error "BAD SECRET ADD"
+#endif
+
 // U64 -> U16 ; 0 -> 1
-static inline u16 hash64_16_01 (u64 hash) {
+static inline u16 xtun_encoding_hash16 (u64 hash) {
     
     hash += hash >> 32;
     hash += hash >> 16;
     hash &= 0xFFFFU;
-    // SE FOR 0, TRANSFORMA EM 1
-    hash += !hash;
 
     return (u16)hash;
 }
 
-static inline u64 xtun_encoding_key_init (u64 sec, u64 key) {
+static inline u64 xtun_encoding_init_key (u64 sec, u64 key) {
 
     key += 0x34342;
     key += sec << (key % 32);
@@ -28,7 +31,7 @@ static inline u64 xtun_encoding_key_init (u64 sec, u64 key) {
     return key;
 }
 
-static inline u64 xtun_encoding_sec_init (u64 sec, u64 key) {
+static inline u64 xtun_encoding_init_sec (u64 sec, u64 key) {
 
     sec += 0x3432;
     sec += key;
@@ -36,14 +39,14 @@ static inline u64 xtun_encoding_sec_init (u64 sec, u64 key) {
     return sec;
 }
 
-static inline u64 xtun_encoding_key_round (u64 sec, u64 key, u8 orig) {
+static inline u64 xtun_encoding_round_key (u64 sec, u64 key, u8 orig) {
 
     key += orig << (sec % 16);
 
     return key;
 }
 
-static inline u64 xtun_encoding_sec_round (u64 sec, u64 key, u8 orig) {
+static inline u64 xtun_encoding_round_sec (u64 sec, u64 key, u8 orig) {
 
     sec += orig   << (sec % 32);
     sec += sec << (orig  % 32);
@@ -52,11 +55,19 @@ static inline u64 xtun_encoding_sec_round (u64 sec, u64 key, u8 orig) {
     return sec;
 }
 
+static inline u64 xtun_encoding_finish_hash (sec, key) {
+
+    // SE FOR 0, TRANSFORMA EM 1
+    sec += !sec;
+    
+    return sec;
+}
+
 // RETORNA: HASH OF SECRET + KEY + ORIGINAL
 static u64 xtun_encode (u64 sec, u64 key, u8* pos, u8* const end) {
 
-    sec = xtun_encoding_sec_init(sec, key);
-    key = xtun_encoding_key_init(sec, key);
+    sec = xtun_encoding_init_sec(sec, key);
+    key = xtun_encoding_init_key(sec, key);
 
     while (pos != end) {
 
@@ -67,39 +78,39 @@ static u64 xtun_encode (u64 sec, u64 key, u8* pos, u8* const end) {
         value ^= key;
         value &= 0xFF;
         value |= 0x100;
-        value -= BYTE_X;
+        value -= XTUN_ENCODING_ORIG_ADD;
         value ^= (value & 0xF) << 4;
         value &= 0xFF;
 
         *pos++ = value;
 
-        sec = xtun_encoding_sec_round(sec, key, value);
-        key = xtun_encoding_key_round(sec, key, value);
+        sec = xtun_encoding_round_sec(sec, key, value);
+        key = xtun_encoding_round_key(sec, key, value);
     }
 
-    return sec;
+    return xtun_encoding_finish_hash(sec, key);
 }
 
 // RETORNA: HASH OF SECRET + KEY + ORIGINAL
 static u64 xtun_decode (u64 sec, u64 key, u8* pos, u8* const end) {
 
-    sec = xtun_encoding_sec_init(sec, key);
-    key = xtun_encoding_key_init(sec, key);
+    sec = xtun_encoding_init_sec(sec, key);
+    key = xtun_encoding_init_key(sec, key);
 
     while (pos != end) {
 
         uint value = *pos;
 
         value ^= (value & 0xF) << 4;
-        value += BYTE_X;
+        value += XTUN_ENCODING_ORIG_ADD;
         value ^= key;
         value &= 0xFF;
 
         *pos++ = value;
 
-        sec = xtun_encoding_sec_round(sec, key, value);
-        key = xtun_encoding_key_round(sec, key, value);
+        sec = xtun_encoding_round_sec(sec, key, value);
+        key = xtun_encoding_round_key(sec, key, value);
     }
 
-    return sec;
+    return xtun_encoding_finish_hash(sec, key);
 }
