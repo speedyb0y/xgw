e5c0d556880544100b5de4538d9a182cb56a8082
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-04 06:30:26 -0300

    INITIAL

--- test-srv.sh
+++ test-srv.sh
@@ -14,12 +14,12 @@ SERVER_IP4=45.76.11.87
     iptables -t raw -A PREROUTING -j DROP   -d ${SERVER_IP4}
     iptables -t raw -A PREROUTING -j DROP   -d ${SERVER_IP4}
 for ID in $(seq 15) ; do
-    iptables -t raw -A PREROUTING -j DROP   -i xgw-${NID} ! -s 172.16.${NID}.0/24
-    iptables -t raw -A PREROUTING -j ACCEPT -i xgw-${NID}   -d 172.16.${NID}.0
+    iptables -t raw -A PREROUTING -j ACCEPT -i xgw-${NID} -s 172.16.${NID}.0/24   -d 172.16.${NID}.0
+    iptables -t raw -A PREROUTING -j ACCEPT -i xgw-${NID} -s 172.16.${NID}.0/24 ! -d 172.16.0.0/16
+    iptables -t raw -A PREROUTING -j DROP   -i xgw-${NID}
 done
     iptables -t raw -A PREROUTING -j DROP -s 172.16.0.0/16
     iptables -t raw -A PREROUTING -j DROP -d 172.16.0.0/16
-
 
 iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o ${SERVER_ITFC} -j MASQUERADE
 
