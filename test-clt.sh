5c5c92da64185467f2af6b0b4b3de2d48b296f49
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-04 04:10:45 -0300

    INITIAL

--- test-clt.sh
+++ test-clt.sh
@@ -10,9 +10,9 @@ ip link set dev xgw up
 # ip -4 rule add table 4 to 172.16.${NODE_ID}.0/24
 # ip -4 route add table 4 dev LAN_INTERFACE src 172.16.${NODE_ID}.20 172.16.${NODE_ID}.0/24
 
-ip -4 rule add table 5 to 172.16.0.0/16
-ip -4 rule add table 5 from 172.16.0.0/16
-ip -4 rule add table 5 oif xgw
+ip -4 rule add metric 5 table 5 to 172.16.0.0/16
+ip -4 rule add metric 5 table 5 from 172.16.0.0/16
+ip -4 rule add metric 5 table 5 oif xgw
 
-ip -4 addr add 172.16.${NODE_ID}.20/24 dev xgw
+ip -4 addr add dev xgw 172.16.${NODE_ID}.20/24 noprefixroute
 ip -4 route add table 5 dev xgw src 172.16.${NODE_ID}.20 default
--- test-srv.sh
+++ test-srv.sh
@@ -2,6 +2,6 @@
 
 for ID in $(seq 15) ; do
     ip link set dev xgw-${ID}  up
-    ip -4 addr add dev xgw-${ID} 172.16.${ID}.0
-    ip -4 route add metric 1 dev xgw-${ID} src 172.16.${ID}.0 172.16.${ID}.0/16
+    ip -4 addr add dev xgw-${ID} 172.16.${ID}.0/24 noprefixroute
+    ip -4 route add metric 1 dev xgw-${ID} src 172.16.${ID}.0 172.16.${ID}.0/24
 done
