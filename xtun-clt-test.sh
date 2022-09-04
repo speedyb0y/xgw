9a21689f0726948e4d6de779c909e0b29fcd3b6c
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-04 06:34:53 -0300

    INITIAL

--- xtun-clt-test.sh
+++ xtun-clt-test.sh
@@ -2,7 +2,7 @@
 
 SERVER_IP4=45.76.11.87
 
-NODE_ID=1
+NID=1
 
 ip link set dev enp5s0 up
 ip link set dev enp6s0 up
@@ -15,18 +15,18 @@ ip -4 rule add priority 2 table isp-1 to ${SERVER_IP4}
 ip -4 rule add priority 2 table isp-2 to ${SERVER_IP4}
 
 # XGW SERVER -> XGW LAN
-ip -4 rule add prioritu 3 table 3 from 172.16.${NODE_ID}.0
-ip -4 rule add prioritu 3 table 3  to  172.16.${NODE_ID}.0/20
+ip -4 rule add prioritu 3 table 3 from 172.16.${NID}.0
+ip -4 rule add prioritu 3 table 3  to  172.16.${NID}.0/20
 ip -4 rule add priority 5 table 5 iif xgw
 
 # XGW LAN -> XGW SERVER
-ip -4 rule add priority 5 table 5  to  172.16.${NODE_ID}.0
-ip -4 rule add priority 5 table 5 from 172.16.${NODE_ID}.0/20
+ip -4 rule add priority 5 table 5  to  172.16.${NID}.0
+ip -4 rule add priority 5 table 5 from 172.16.${NID}.0/20
 ip -4 rule add priority 5 table 5 oif xgw
 
 # NOTE: SE VAMOS FAZER FORWARDING, ENTAO
-# ip -4 rule add table 4 to 172.16.${NODE_ID}.0/24
-# ip -4 route add table 4 dev LAN_INTERFACE src 172.16.${NODE_ID}.20 172.16.${NODE_I>
+# ip -4 rule add table 4 to 172.16.${NID}.0/24
+# ip -4 route add table 4 dev LAN_INTERFACE src 172.16.${NID}.20 172.16.${NODE_I>
 
-ip -4 addr add dev xgw 172.16.${NODE_ID}.20/24 noprefixroute
-ip -4 route add table 5 dev xgw src 172.16.${NODE_ID}.20 default
+ip -4 addr add dev xgw 172.16.${NID}.20/24 noprefixroute
+ip -4 route add table 5 dev xgw src 172.16.${NID}.20 default
