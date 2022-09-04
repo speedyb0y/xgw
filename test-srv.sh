54c4cf3d235b8bed44778aecd432603211cd838b
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-04 06:16:45 -0300

    INITIAL

--- test-srv.sh
+++ test-srv.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+SERVER_ITFC=enp1s0
+
+SERVER_IP4=45.76.11.87
+
+    iptables -t raw -A PREROUTING -j ACCEPT -i ${SERVER_ITFC} -d ${SERVER_IP4}
+    iptables -t raw -A PREROUTING -j DROP   -i ${SERVER_ITFC}
+    iptables -t raw -A PREROUTING -j DROP                     -d ${SERVER_IP4}
 for ID in $(seq 15) ; do
-    iptables -t raw -A PREROUTING -j ACCEPT -s 172.16.${ID}.0/24   -d 172.16.${ID}.0/24 -i xgw-${ID}
-    iptables -t raw -A PREROUTING -j ACCEPT -s 172.16.${ID}.0/24 ! -d 172.16.0.0/16     -i xgw-${ID}
+    iptables -t raw -A PREROUTING -j ACCEPT -s 172.16.${NID}.0/24   -d 172.16.${NID}.0/24 -i xgw-${NID}
+    iptables -t raw -A PREROUTING -j ACCEPT -s 172.16.${NID}.0/24 ! -d 172.16.0.0/16     -i xgw-${NID}
 done
     iptables -t raw -A PREROUTING -j DROP   -s 172.16.0.0/16
     iptables -t raw -A PREROUTING -j DROP   -d 172.16.0.0/16
 
-iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o enp1s0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o ${SERVER_ITFC} -j MASQUERADE
 
-for ID in $(seq 15) ; do
-    echo 1 > /proc/sys/net/ipv4/conf/xgw-${ID}/forwarding
+for NID in $(seq 15) ; do
+    echo 1 > /proc/sys/net/ipv4/conf/xgw-${NID}/forwarding
 done
 
-for ID in $(seq 15) ; do
-    ip link set dev xgw-${ID}  up
-    ip -4 addr add dev xgw-${ID} 172.16.${ID}.0/24 noprefixroute
-    ip -4 route add metric 1 dev xgw-${ID} src 172.16.${ID}.0 172.16.${ID}.0/24
+for NID in $(seq 15) ; do
+    ip link set dev xgw-${NID}  up
+    ip -4 addr add dev xgw-${NID} 172.16.${NID}.0/24 noprefixroute
+    ip -4 route add metric 1 dev xgw-${NID} src 172.16.${NID}.0 172.16.${NID}.0/24
 done
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
