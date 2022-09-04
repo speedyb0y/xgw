abd93e0b9f2ce707e9cd26902d9530a5e8b0003e
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-04 04:25:43 -0300

    INITIAL

--- test-srv.sh
+++ test-srv.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 for ID in $(seq 15) ; do
+    iptables -t raw -A PREROUTING -j ACCEPT -s 172.16.${ID}.0/24   -d 172.16.${ID}.0/24 -i xgw-${ID}
+    iptables -t raw -A PREROUTING -j ACCEPT -s 172.16.${ID}.0/24 ! -d 172.16.0.0/16     -i xgw-${ID}
+done
+    iptables -t raw -A PREROUTING -j DROP   -s 172.16.0.0/16
+    iptables -t raw -A PREROUTING -j DROP   -d 172.16.0.0/16
+
+iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o enp1s0 -j MASQUERADE
+
+for ID in $(seq 15) ; do
+    echo 1 > /proc/sys/net/ipv4/conf/xgw-${ID}/forwarding
+done
+
+for ID in $(seq 15) ; do
     ip link set dev xgw-${ID}  up
     ip -4 addr add dev xgw-${ID} 172.16.${ID}.0/24 noprefixroute
     ip -4 route add metric 1 dev xgw-${ID} src 172.16.${ID}.0 172.16.${ID}.0/24
 done
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
