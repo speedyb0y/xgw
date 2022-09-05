4311d909ec68e5ff88c498cffbd87ef4a3101302
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2022-09-05 10:29:33 -0300

    INITIAL

--- kmod/crypto.c
+++ kmod/crypto.c
@@ -45,7 +45,10 @@ static inline u64 unswap64 (u64 x, const u64 mask) {
 #define XGW_CRYPTO_PARAMS_SIZE 32
 
 typedef union xgw_crypto_params_s {
-         u8 _[XGW_CRYPTO_PARAMS_SIZE];
+        u8 w8[XGW_CRYPTO_PARAMS_SIZE/sizeof(u8)];
+      u16 w16[XGW_CRYPTO_PARAMS_SIZE/sizeof(u16)];
+      u32 w32[XGW_CRYPTO_PARAMS_SIZE/sizeof(u32)];
+      u64 w64[XGW_CRYPTO_PARAMS_SIZE/sizeof(u64)];
 #if XCONF_XGW_CRYPTO_ALGO_NULL0
     // NOTHING
 #endif
